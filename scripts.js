03505822a379d2c00784475a2ae36b940aa05890
Matthew Jamison <matthewjamison@Matthews-MacBook-Pro.local>
2018-09-24 16:53:36 -0500

    Store Cards

--- scripts.js
+++ scripts.js
@@ -1,7 +1,29 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
   this.classList.toggle('flip');
+
+  
+  if(!hasFlippedCard) {
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    // second click
+    hasFlippedCard = false;
+    secondCard = this;
+
+    // do cards match?
+    if(firstCard.dataset.framework === secondCard.dataset.framework) {
+      console.log ('its a match');
+    } else {
+      console.log ('no match');
+    }
+
+  }
 }
 
 
