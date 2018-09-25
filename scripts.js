ec7d9d7d1cf052fa0d22ac59bad605545efe63c9
Matthew Jamison <matthewjamison@Matthews-MacBook-Pro.local>
2018-09-25 08:39:28 -0500

    Refactoring

--- scripts.js
+++ scripts.js
@@ -5,32 +5,34 @@ let firstCard, secondCard;
 
 function flipCard() {
   this.classList.toggle('flip');
-
-  
   if(!hasFlippedCard) {
     // first click
     hasFlippedCard = true;
     firstCard = this;
-  } else {
-    // second click
-    hasFlippedCard = false;
-    secondCard = this;
-
-    // do cards match?
-    if(firstCard.dataset.framework === secondCard.dataset.framework) {
-      // its a match!
-      firstCard.removeEventListener('click', flipCard);
-      secondCard.removeEventListener('click', flipCard);
-    } else {
-      // not a match
-      setTimeout(() => {
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
-      }, 1500);
-    }
+    return;
   }
+  // second click
+  hasFlippedCard = false;
+  secondCard = this;
+  checkForMatch();
 }
 
+function checkForMatch() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+  isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards(){
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards(){
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+  }, 1500);
+}
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
