0d210cf7456f560a8c67a85a712cada2d2274a67
Matthew Jamison <matthewjamison@Matthews-MacBook-Pro.local>
2018-09-26 12:41:25 -0500

    Shuffling

--- scripts.js
+++ scripts.js
@@ -24,12 +24,13 @@ function checkForMatch() {
   isMatch ? disableCards() : unflipCards();
 }
 
-function disableCards(){
+function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
+  resetBoard();
 }
 
-function unflipCards(){
+function unflipCards() {
   lockBoard = true;
 
   setTimeout(() => {
@@ -37,8 +38,26 @@ function unflipCards(){
     secondCard.classList.remove('flip');
 
     lockBoard = false;
+    resetBoard();
   }, 1500);
 }
+
+function resetBoard() {
+  // Uses ES6 destructuring assignment
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+// This is an IIFE, will be executed right after its definition
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    // apply random number to order property
+    card.style.order = randomPos;
+  })
+})();
+
+
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
